0905ad105aef991b9e9e10cfaea70d8d4c94087f
Alexandre de Souza <alexandre@aledsz.com.br>
2020-03-17 02:07:36 -0300

    Add MSMQ Messenger class

---
+++ BhakOffice.Core/Queues/Messenger.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Messaging;
+using BhakOffice.Types;
+
+namespace BhakOffice.Core.Queues {
+  public class Messenger {
+    protected MessageQueue _queue = new MessageQueue();
+    private static readonly BinaryMessageFormatter _formatter = new BinaryMessageFormatter();
+
+    private Messenger(String queue_name) {
+      this._queue = new MessageQueue(queue_name);
+    }
+
+    public static Messenger GetNotificationsMessenger() {
+      return new Messenger(Global.NotificationsQueue);
+    }
+
+    public Response Send(String topic, Object payload) {
+      using (var transaction = new MessageQueueTransaction()) {
+        try {
+          transaction.Begin();
+          var message = new QueueMessage() { topic = topic, payload = payload };
+          this._queue.Send(new Message(message, _formatter), topic, MessageQueueTransactionType.Single);
+          transaction.Commit();
+
+          return new Response(Returns.OK, message);
+        }
+        catch (Exception ex) {
+          return new Response(Returns.Error, ex.Message);
+        }
+      }
+    }
+  }
+}
