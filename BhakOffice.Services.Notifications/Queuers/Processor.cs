13b4ab454098ef82f21e0f7b51db0e2f74c0abbf
Alexandre de Souza <alexandre@aledsz.com.br>
2020-03-17 04:20:03 -0300

    Add queue processor class

---
+++ BhakOffice.Services.Notifications/Queuers/Processor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BhakOffice.Core.Queues;
+using BhakOffice.Types;
+
+namespace BhakOffice.Services.Notifications.Queuers {
+  public class Processor {
+    public static void Run(MSMQ queuer, QueueMessage message) {
+      if (message.topic == "product_created") {
+        queuer.Acknowledge(message.id);
+      }
+    }
+  }
+}
