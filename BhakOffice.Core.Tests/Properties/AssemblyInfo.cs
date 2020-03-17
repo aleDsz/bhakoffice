7f31a9f082adfd3c079b925d4aae05fc6e62316b
Alexandre de Souza <alexandre@aledsz.com.br>
2020-03-17 02:07:59 -0300

    Add BhakOffice Core tests

---
+++ BhakOffice.Core.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BhakOffice.Core.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BhakOffice.Core.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5b45541f-f558-41ec-9600-edcecbd9a5df")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ BhakOffice.Core.Tests/SetupTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BhakOffice.Core.Tests {
+  [TestClass]
+  public class SetupTests {
+
+    [TestCategory("Migration")]
+    [TestMethod]
+    public void TestSucessInstall() {
+      var result = BhakOffice.Core.Setup.Install();
+      Assert.AreEqual(true, result.IsSuccess());
+    }
+
+    [TestCategory("Migration")]
+    [TestMethod]
+    public void TestFailInstall() {
+      var result = BhakOffice.Core.Setup.Install(BhakOffice.Types.Returns.Error);
+      Assert.AreEqual(false, result.IsSuccess());
+    }
+  }
+}
