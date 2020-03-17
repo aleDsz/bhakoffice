cf5eecb19b79e4341ea7a59ff313b061936ed3e9
Alexandre de Souza <alexandre@aledsz.com.br>
2020-03-17 02:04:45 -0300

    Add Queue Message class

---
+++ BhakOffice.Types/QueueMessage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BhakOffice.Types {
+
+  [Serializable]
+  public class QueueMessage {
+    public String id { get; set; }
+    public String topic { get; set; }
+    public Object payload { get; set; }
+  }
+}
